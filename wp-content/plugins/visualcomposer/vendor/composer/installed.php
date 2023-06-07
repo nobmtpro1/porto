039b477c74e032aee59d0d3ff5bcba6290f32d5a
Thai Nguyen <thai.nguyen@aegona.com>
2023-06-07 19:03:08 +0700

    #

---
+++ wp-content/plugins/visualcomposer/vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '45.2.x-dev',
+    'version' => '45.2.9999999.9999999-dev',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '7d1f41397e1d0f16f76949517ee91ed6e795da80',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '45.2.x-dev',
+      'version' => '45.2.9999999.9999999-dev',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '7d1f41397e1d0f16f76949517ee91ed6e795da80',
+    ),
+  ),
+);
